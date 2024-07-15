df0d7f0184dd891e216672683db8030789227103
erika-lee-site <169239308+erika-lee-site@users.noreply.github.com>
2024-07-15 00:10:01 -0700

    google maps API example

---
+++ week7/google-maps/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ReactGoogleMapComponent from './ReactGoogleMap'
+
+
+function App() {
+  return (
+    <div>
+      <ReactGoogleMapComponent/>
+    </div>
+  )
+}
+
+export default App
